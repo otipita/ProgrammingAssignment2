4b25d37d60819a1e3f5eebd51a0b203a4ce55af2
otipita <otipita@gmail.com>
2015-02-20 20:01:54 +0200

    inerse matrix finding

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##There functions serve to make inverse matrix calculations less time consuming
+##by saving result of calculation in cache and use it once it is needed in sted of calculation
 
-## Write a short comment describing this function
+##Function makeCacheMatrix making inverse matrix from matrix X and writing result in cache
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 }
 
 
-## Write a short comment describing this function
+##This function checking if there is result of inverse matrix in Cache
+## if there is no result in cache function request calculation of the inverse matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
